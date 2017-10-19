705fd1be7809711aba72d2a6657ffa88b9f9d6da
Group 2 <alfredsson.d@gmail.com>
2017-10-20 01:04:50 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -17,6 +17,9 @@ var video = document.getElementById("video");
 var videoPlay = false;
 var windowWidth = window.innerWidth;
 var windowHeight = window.innerHeight;
+var quizStarted = false;
+var currentScore = 0;
+var quizClicks = 0;
 
 //function that toggles the menu
 function toggleMenu(x) {
@@ -33,6 +36,7 @@ function closeMenu() {
   $("#barContainer").children().toggleClass("rainbowGradientAnimated");
   $("#barContainer").children().toggleClass("clipBox");
   document.getElementById("menu").classList.toggle("hidden");
+  document.body.classList.toggle("noScroll");
 }
 
 //function that pauses or plays video
@@ -74,77 +78,148 @@ function videoWidth() {
 function checkAnswer(el, x) {
   console.log("running checkAnswer...");
 
-  //rounding nr to identify case
+  //calculate x to get which quiz initiated + clicked answer
   var question = Math.floor(x);
+  var answer = String(x).slice(-1);
+  console.log("answer: " + answer);
   //clicked element container
   var container = el.parentElement;
   //which chldren to remove
   var childrenToRemove;
   //correct answer
-  var correctAnswer;
+  var correctElement;
 
   $(container.children).removeClass("grow");
 
   switch (question) {
     case 1:
-      correctAnswer = $(container).children(".answer:nth-child(1)");
-      // console.log(correctAnswer);
-      childrenToRemove = $(container).children().not(correctAnswer);
+      correctAnswer = 1;
+      correctElement = $(container).children(".answer:nth-child(" + correctAnswer + ")");
+      console.log(correctElement);
+      childrenToRemove = $(container).children().not(correctElement);
       // console.log(childrenToRemove);
-      removeChild(container, correctAnswer, childrenToRemove);
+      removeChild(container, correctElement, childrenToRemove);
+      if (answer == correctAnswer) {
+        quizScore(1, question);
+      } else {
+        quizScore(0, question);
+      }
       break;
     case 2:
-      correctAnswer = $(container).children(".answer:nth-child(3)");
-      // console.log(correctAnswer);
-      childrenToRemove = $(container).children().not(correctAnswer);
+      correctAnswer = 3;
+      correctElement = $(container).children(".answer:nth-child(" + correctAnswer + ")");
+      // console.log(correctElement);
+      childrenToRemove = $(container).children().not(correctElement);
       // console.log(childrenToRemove);
-      removeChild(container, correctAnswer, childrenToRemove);
+      removeChild(container, correctElement, childrenToRemove);
+      if (answer == correctAnswer) {
+        quizScore(1, question);
+      } else {
+        quizScore(0, question);
+      }
       break;
     case 3:
-      correctAnswer = $(container).children(".answer:nth-child(2)");
-      // console.log(correctAnswer);
-      childrenToRemove = $(container).children().not(correctAnswer);
+      correctAnswer = 3;
+      correctElement = $(container).children(".answer:nth-child(" + correctAnswer + ")");
+      // console.log(correctElement);
+      childrenToRemove = $(container).children().not(correctElement);
       // console.log(childrenToRemove);
-      removeChild(container, correctAnswer, childrenToRemove);
+      removeChild(container, correctElement, childrenToRemove);
+      if (answer == correctAnswer) {
+        quizScore(1, question);
+      } else {
+        quizScore(0, question);
+      }
+      break;
+    case 4:
+      correctAnswer = 2;
+      correctElement = $(container).children(".answer:nth-child(" + correctAnswer + ")");
+      // console.log(correctElement);
+      childrenToRemove = $(container).children().not(correctElement);
+      // console.log(childrenToRemove);
+      removeChild(container, correctElement, childrenToRemove);
+      console.log("answer: " + answer);
+      if (answer == correctAnswer) {
+        quizScore(1, question);
+      } else {
+        quizScore(0, question);
+      }
       break;
     default:
 
   }
 }
 
-function removeChild(container, correctAnswer, childrenToRemove) {
+//function that create score for quiz
+function quizScore(correct, nr) {
+  //check if this is first question answered
+  if (quizStarted === false) {
+    var score = document.getElementById("scoreContainer");
+    quizStarted = true;
+    if (score.classList.contains("hidden")) {
+      score.classList.remove("hidden");
+    }
+  }
+  console.log("correct:" + correct);
+  var scores = document.querySelectorAll("#score .score");
+  nr = nr - 1;
+  var clickedElement = scores[(nr)];
+
+  if (correct == 1) {
+    console.log("adding to score");
+    currentScore++;
+    console.log(currentScore);
+    //function that waits 400 millis and add to score
+    window.setTimeout(function() {
+      document.getElementById("currentScore").innerHTML = currentScore;
+      clickedElement.classList.add("correctAnswer");
+    }, 400);
+    console.log(scores);
+    console.log(clickedElement);
+  }
+  else {
+    window.setTimeout(function() {
+      document.getElementById("currentScore").innerHTML = currentScore;
+      clickedElement.classList.add("wrongAnswer");
+    }, 400);
+  }
+}
+
+//function that animates and removes quiz answers that aren't correct
+function removeChild(container, correctElement, childrenToRemove) {
   console.log(childrenToRemove);
 
-  $(correctAnswer).css("background-color", "lightgreen");
-  
+  $(correctElement).css("background-color", "lightgreen");
+
   var relativeOffset = anime.timeline();
 
   relativeOffset
-  .add({
-    targets: childrenToRemove[0],
-    opacity: 0,
-    duration: 400,
-    easing: 'easeOutExpo',
-  })
-  .add({
-    targets: childrenToRemove[1],
-    opacity: 0,
-    duration: 400,
-    easing: 'easeOutExpo',
-    offset: '-=200' // Starts 600ms before the previous animation ends
-  })
-  .add({
-    targets: childrenToRemove[2],
-    opacity: 0,
-    duration: 400,
-    easing: 'easeOutExpo',
-    offset: '-=400' // Starts 800ms before the previous animation ends
-  })
-  .add({
-    targets: [childrenToRemove],
-    display: 'none'
-  });
+    .add({
+      targets: childrenToRemove[0],
+      opacity: 0,
+      duration: 400,
+      easing: 'easeOutExpo',
+    })
+    .add({
+      targets: childrenToRemove[1],
+      opacity: 0,
+      duration: 400,
+      easing: 'easeOutExpo',
+      offset: '-=200' // Starts 600ms before the previous animation ends
+    })
+    .add({
+      targets: childrenToRemove[2],
+      opacity: 0,
+      duration: 400,
+      easing: 'easeOutExpo',
+      offset: '-=400' // Starts 800ms before the previous animation ends
+    })
+    .add({
+      targets: [childrenToRemove],
+      display: 'none'
+    });
 
+  // REMOVE CHILDREN AFTER FADING OUT
   var promise = relativeOffset.finished.then(logFinished);
 
   function logFinished() {
@@ -153,21 +228,4 @@ function removeChild(container, correctAnswer, childrenToRemove) {
     }
   }
 
-//   var JSarray = anime({
-//   targets: [childrenToRemove],
-//   display: 'none'
-// });
-
-}
-
-function correctAnswer(el) {
-  var container = el.parentNode;
-  // console.log(container);
-  var answers = container.children;
-  console.log(answers);
-  for (var i = 0; i < answers.length; i++) {
-    // console.log(i + answers[i]);
-    // console.log(i + container.children[i]);
-    console.log("correct answer: " + i + container.children[i].classList.contains("correctAnswer"));
-  }
 }
